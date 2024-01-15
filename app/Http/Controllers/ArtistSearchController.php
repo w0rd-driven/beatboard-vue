2e53e1c6260b665152c9598dc4355c6dc12985ad
Jeremy Brayton <w0rd-driven@users.noreply.github.com>
2024-01-15 14:08:27 -0500

    Artist model stubbing
    Generated from the following commands:
    
    `./artisan make:model Artist -f -m -s`
    `./artisan make:controller -i --pest ArtistSearchController`
    `./artisan make:request ArtistSearchRequest`
    
    Wire up a basic controller and request behavior as a jumping-off point. Now I'll start playing with Spotify data to see what is useful to cache locally in the database.
    
    TODO:
    Migration
    Model
    Factory
    Seeder

---
+++ app/Http/Controllers/ArtistSearchController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ArtistSearchRequest;
+
+class ArtistSearchController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(ArtistSearchRequest $request)
+    {
+        //
+    }
+}
